a44a07370659386c5629e8231e8864eea3d62b6a
Ivory Tang <veive@mac.mynetworksettings.com>
2026-01-08 22:58:41 -0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
+    // do not remove these
     react(),
     tailwindcss(),
   ],
